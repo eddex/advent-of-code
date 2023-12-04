187d491d352c0d1486249dca37bdc1fba7ca1eff
eddex <eddex@outlook.com>
2023-12-04 17:13:28 +0100

    2023 day 3 part 1

---
+++ 2023/Day3.cs
@@ -0,0 +1,75 @@
+﻿namespace _2023;
+
+public static class Day3
+{
+    public static string Solve1(string inputPath)
+    {
+        var map = File.ReadLines(inputPath).ToArray();
+        var mapHeight = map.Length;
+        var mapWidth = map[0].Length;
+
+        var numbersAdjacentToSymbol = new List<int>();
+        for (var h = 0; h < mapHeight; h++)
+        {
+            var number = string.Empty;
+            var hasAdjacentSymbol = false;
+            for (var w = 0; w < mapWidth; w++)
+            {
+                var field = map[h][w];
+                if (char.IsNumber(field))
+                {
+                    number += field;
+                    hasAdjacentSymbol = hasAdjacentSymbol || HasAdjacentSymbol(h, w, map, mapHeight, mapWidth);
+                    if (w == mapWidth-1 && hasAdjacentSymbol)
+                    {
+                        numbersAdjacentToSymbol.Add(int.Parse(number));
+                        number = string.Empty;
+                        hasAdjacentSymbol = false;
+                    }
+                }
+                else
+                {
+                    if (number != string.Empty)
+                    {
+                        if (hasAdjacentSymbol)
+                        {
+                            numbersAdjacentToSymbol.Add(int.Parse(number));
+                        }
+                        number = string.Empty;
+                        hasAdjacentSymbol = false;
+                    }
+                }
+
+            }
+        }
+
+        return numbersAdjacentToSymbol.Sum().ToString();
+    }
+
+    public static string Solve2(string inputPath)
+    {
+        return "todo";
+    }
+
+    private static bool HasAdjacentSymbol(int h, int w, string[] map, int mapHeight, int mapWidth)
+    {
+        return
+            CheckPositionForSymbol(h - 1, w, map, mapHeight, mapWidth) ||     // top
+            CheckPositionForSymbol(h - 1, w - 1, map, mapHeight, mapWidth) || // top left
+            CheckPositionForSymbol(h - 1, w + 1, map, mapHeight, mapWidth) || // top right
+            CheckPositionForSymbol(h + 1, w, map, mapHeight, mapWidth) ||     // bottom
+            CheckPositionForSymbol(h + 1, w - 1, map, mapHeight, mapWidth) || // bottom left
+            CheckPositionForSymbol(h + 1, w + 1, map, mapHeight, mapWidth) || // bottom right
+            CheckPositionForSymbol(h, w - 1, map, mapHeight, mapWidth) ||     // left
+            CheckPositionForSymbol(h, w + 1, map, mapHeight, mapWidth);       // right
+    }
+
+    private static bool CheckPositionForSymbol(int h, int w, string[] map, int mapHeight, int mapWidth)
+    {
+        if (h < 0 || h >= mapHeight || w < 0 || w >= mapWidth)
+            return false;
+        return IsSymbol(map[h][w]);
+    }
+
+    private static bool IsSymbol(char c) => c != '.' && !char.IsNumber(c);
+}
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -10,3 +10,8 @@ Console.WriteLine("Day 2 test 1: " + Day2.Solve1("./input/day2-test.txt"));
 Console.WriteLine("Day 2 part 1: " + Day2.Solve1("./input/day2.txt"));
 Console.WriteLine("Day 2 test 2: " + Day2.Solve2("./input/day2-test.txt"));
 Console.WriteLine("Day 2 part 2: " + Day2.Solve2("./input/day2.txt"));
+
+Console.WriteLine("Day 3 test 1: " + Day3.Solve1("./input/day3-test.txt"));
+Console.WriteLine("Day 3 part 1: " + Day3.Solve1("./input/day3.txt"));
+Console.WriteLine("Day 3 test 2: " + Day3.Solve2("./input/day3-test.txt"));
+Console.WriteLine("Day 3 part 2: " + Day3.Solve2("./input/day3.txt"));
