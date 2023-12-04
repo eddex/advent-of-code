5312ee974df0bcaeeddbb6b3000c60698168bfdd
eddex <eddex@outlook.com>
2023-12-04 17:12:40 +0100

    2023 day 1

---
+++ 2023/Day1.cs
@@ -0,0 +1,41 @@
+﻿namespace _2023;
+
+public static class Day1
+{
+    public static string Solve(string inputPath)
+    {
+        using var sr = new StreamReader(inputPath);
+
+        var numbers = new List<int>();
+        var textNumbers = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            char? first = null;
+            char? last = null;
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                if (char.IsNumber(c))
+                    if (first == null)
+                        first = last = c;
+                    else
+                        last = c;
+                for (var j = 0; j < textNumbers.Length; j++)
+                {
+                    if (line.Substring(i, line.Length-i).StartsWith(textNumbers[j]))
+                    {
+                        c = (j + 1).ToString()[0];
+                        if (first == null)
+                            first = last = c;
+                        else
+                            last = c;
+                    }
+                }
+            }
+            numbers.Add(int.Parse($"{first}{last}"));
+        }
+        return numbers.Sum().ToString();
+    }
+}
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -1,3 +1,7 @@
 ﻿using _2023;
 
 Console.WriteLine("Advent of Code 2023!");
+
+Console.WriteLine("Day 1 test 1: " + Day1.Solve("./input/day1-test1.txt"));
+Console.WriteLine("Day 1 test 2: " + Day1.Solve("./input/day1-test2.txt"));
+Console.WriteLine("Day 1: " + Day1.Solve("./input/day1.txt"));
